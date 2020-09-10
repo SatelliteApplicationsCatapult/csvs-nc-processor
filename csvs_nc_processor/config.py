57058f9a86d30a1cf01e9d37e2a7963026a53cae
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-09-10 17:33:55 +0100

    Refactor code

--- csvs_nc_processor/config.py
+++ csvs_nc_processor/config.py
@@ -11,6 +11,6 @@ s3_id = os.getenv('S3_ID', 'id')
 s3_key = os.getenv('S3_KEY', 'key')
 
 tmp_dir = pathlib.Path('./.tmp')
-input_tar_file = pathlib.Path(os.getenv('INPUT_TAR_FILE', './resources/ERA5_daily_mean_2mTemp_1980.tar.gz'))
+input_tar_file = pathlib.Path(os.getenv('INPUT_TAR_FILE', '../resources/ERA5_daily_mean_2mTemp_1980.tar.gz'))
 output_nc_file = pathlib.Path(os.getenv('OUTPUT_NC_FILE', './' + input_tar_file.name.split('.')[0] + '.nc'))
 std_name = os.getenv('STD_NAME', 'air_temperature')
