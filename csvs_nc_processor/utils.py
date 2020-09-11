36648335b09e5a724172c089803a31113c9a4f1f
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-09-11 14:57:22 +0100

    Fix root path conversion in absolute paths

--- csvs_nc_processor/utils.py
+++ csvs_nc_processor/utils.py
@@ -5,5 +5,7 @@ def path_to_str(path: pathlib.Path) -> str:
     path_str = []
     for p in path.parts:
         path_str.append(p)
-        path_str.append('/')
+        if p != '/':
+            path_str.append('/')
     return ''.join(path_str[:-1])
+
