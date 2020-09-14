cabcee137fbb376fe7e45b016dbb06046a65baf5
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-09-14 14:52:59 +0100

    Refactor downloader structure and names

--- csvs_nc_processor/downloader.py
+++ csvs_nc_processor/downloader.py
@@ -8,20 +8,17 @@ logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
 logger = logging.getLogger(__name__)
 
 
-def obtain_files() -> [pathlib.Path, pathlib.Path]:
+def obtain_input_output_files() -> [pathlib.Path, pathlib.Path]:
     for t in climate_data_to_download:
         for d in climate_data_to_download[t]:
             for y in climate_data_interval:
-                filename = d + '_' + str(y) + file_type
-                file = download_file(filename, t)
-                output_nc_file = pathlib.Path(file.name.split('.')[0] + '.nc')
-                yield file, output_nc_file
+                filename = obtain_filename(d, y)
+                yield download_file(filename, t), create_output_file(filename)
 
 
-def download_file(filename: str, t: str) -> pathlib.Path:
-    file = tmp_dir / filename
-    file_url = climate_data_url + t + '/' + filename
-    tmp_dir.mkdir(parents=True, exist_ok=True)
+def download_file(filename: str, data_type: str) -> pathlib.Path:
+    file = create_tmp_file(filename)
+    file_url = obtain_url_to_download(filename, data_type)
 
     logger.info(f'Downloading {file_url}...')
     r = requests.get(file_url)
@@ -33,3 +30,21 @@ def download_file(filename: str, t: str) -> pathlib.Path:
         logger.warning(f'Could not download {file_url}...')
 
     return file
+
+
+def obtain_filename(data_name: str, data_year: str) -> str:
+    return data_name + '_' + str(data_year) + file_type
+
+
+def obtain_url_to_download(filename, data_type) -> str:
+    return climate_data_url + data_type + '/' + filename
+
+
+def create_tmp_file(filename: str) -> pathlib.Path:
+    tmp_dir.mkdir(parents=True, exist_ok=True)
+    return tmp_dir / filename
+
+
+def create_output_file(filename: str) -> pathlib.Path:
+    output_name = filename.split('.')[0] + '.nc'
+    return tmp_dir / output_name
