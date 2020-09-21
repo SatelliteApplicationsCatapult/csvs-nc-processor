81ea5e5f5bc36c85ed3fc23183517c894da73de5
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-09-21 13:23:12 +0100

    Add new functions to load and save states in persit state service
    - Add load_state function
    - Add get_state function
    - Modify save_state

--- services/persist_state_service.py
+++ services/persist_state_service.py
@@ -29,13 +29,8 @@ def update_failed_uploads(nc_file_path: str) -> List[str]:
     return failed_uploads
 
 
-def save_state() -> str:
-    state = {
-        'failed_downloads': failed_downloads,
-        'failed_processes': failed_processes,
-        'failed_uploads': failed_uploads
-    }
-    state_file_name = f'state_{datetime.now().strftime("%Y-%m-%d_%H.%M.%S")}.json'
+def save_state(state: Dict[str, List]) -> str:
+    state_file_name = f'state_{datetime.now().strftime("%Y-%m-%d_%H꞉%M꞉%S.%f")}.json'
     return create_state_file(state, state_file_name)
 
 
@@ -45,3 +40,32 @@ def create_state_file(state: Dict[str, List], filename: str) -> str:
     with open(state_file, 'w') as f_out:
         json.dump(state, f_out)
     return str(state_file)
+
+
+def load_state(state_file_path: str) -> Dict[str, List]:
+    with open(state_file_path, "r") as read_file:
+        state = json.load(read_file)
+    if not valid_state(state):
+        raise InvalidStateFile(f'{state_file_path} is not a valid state file.')
+    return state
+
+
+def get_state():
+    return {
+        'failed_downloads': failed_downloads,
+        'failed_processes': failed_processes,
+        'failed_uploads': failed_uploads
+    }
+
+
+def valid_state(state: Dict[str, List]) -> bool:
+    valid_state_keys = ['failed_downloads', 'failed_processes', 'failed_uploads']
+    if list(state.keys()) == valid_state_keys:
+        return True
+    else:
+        return False
+
+
+class InvalidStateFile(Exception):
+    """Base class for exceptions in this module."""
+    pass
--- tests/test_persist_state_service.py
+++ tests/test_persist_state_service.py
@@ -1,8 +1,21 @@
+import json
 import pathlib
 import unittest
 
 from services.persist_state_service import update_failed_downloads, update_failed_processes, update_failed_uploads, \
-    save_state
+    save_state, load_state, get_state, InvalidStateFile
+
+state_example = {
+    'failed_downloads': [],
+    'failed_processes': [],
+    'failed_uploads': []
+}
+
+dataset_url = 'url/to/dataset.tar.gz'
+valid_tar_file = 'tests/resources/ERA5_daily_mean_2mTemp_1979.tar.gz'
+invalid_tar_file = 'tests/resources/non_existing.tar.gz'
+valid_nc_file = 'tests/resources/ERA5_daily_mean_2mTemp_1979.nc'
+invalid_nc_file = 'tests/resources/non_existing.nc'
 
 
 class TestPersistStateService(unittest.TestCase):
@@ -15,31 +28,51 @@ class TestPersistStateService(unittest.TestCase):
             pathlib.Path(file).unlink()
 
     def test_update_failed_download(self):
-        file_url = 'url/to/file'
-        failed_downloads = update_failed_downloads(download_url=file_url)
-        assert file_url in failed_downloads
+        failed_downloads = update_failed_downloads(download_url=dataset_url)
+        assert dataset_url in failed_downloads
 
     def test_update_failed_process(self):
-        filename = 'tests/resources/ERA5_daily_mean_2mTemp_1979.tar.gz'
-        failed_processes = update_failed_processes(tar_file_path=filename)
-        assert filename in failed_processes
+        failed_processes = update_failed_processes(tar_file_path=valid_tar_file)
+        assert valid_tar_file in failed_processes
 
     def test_update_failed_upload(self):
-        filename = 'tests/resources/ERA5_daily_mean_2mTemp_1979.nc'
-        failed_uploads = update_failed_uploads(nc_file_path=filename)
-        assert filename in failed_uploads
+        failed_uploads = update_failed_uploads(nc_file_path=valid_nc_file)
+        assert valid_nc_file in failed_uploads
 
     def test_update_non_existing_process(self):
-        filename = 'tests/resources/non_existing.tar.gz'
-        failed_processes = update_failed_processes(tar_file_path=filename)
-        assert filename not in failed_processes
+        failed_processes = update_failed_processes(tar_file_path=invalid_tar_file)
+        assert invalid_tar_file not in failed_processes
 
     def test_update_non_existing_upload(self):
-        filename = 'file_name_path'
-        failed_uploads = update_failed_uploads(nc_file_path=filename)
-        assert filename not in failed_uploads
+        failed_uploads = update_failed_uploads(nc_file_path=invalid_nc_file)
+        assert invalid_nc_file not in failed_uploads
 
     def test_save_state(self):
-        created_file = save_state()
+        created_file = save_state(state_example)
         self.tmp_test_files.append(created_file)
         assert pathlib.Path(created_file).exists()
+
+    def test_save_failed_uploads_state(self):
+        update_failed_uploads(nc_file_path=valid_nc_file)
+        state = get_state()
+        created_file = save_state(state)
+        self.tmp_test_files.append(created_file)
+        with open(created_file, "r") as read_file:
+            state = json.load(read_file)
+        assert valid_nc_file in state.get('failed_uploads')
+
+    def test_load_state(self):
+        created_file = save_state(state_example)
+        self.tmp_test_files.append(created_file)
+        state = load_state(created_file)
+        assert state.keys() == state_example.keys()
+
+    def test_load_state_invalid_file(self):
+        wrong_state_structure = {
+            'something': [],
+            'wrong': []
+        }
+        created_file = save_state(wrong_state_structure)
+        self.tmp_test_files.append(created_file)
+        with self.assertRaises(InvalidStateFile):
+            load_state(created_file)
